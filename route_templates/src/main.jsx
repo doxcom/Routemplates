3e782cb7405429bb3aef413eb8d70b8189bd48bd
doxcom <aldoperez1319@gmail.com>
2026-07-14 16:26:36 -0600

    adding routes part

--- route_templates/src/main.jsx
+++ route_templates/src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
   </React.StrictMode>,
