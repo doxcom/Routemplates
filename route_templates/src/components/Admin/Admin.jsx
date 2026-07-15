dbc056bfb125088471cb48d905956582be5d055d
doxcom <aldoperez1319@gmail.com>
2026-07-15 14:50:24 -0600

    refactoring routes part

--- route_templates/src/components/Admin/Admin.jsx
+++ route_templates/src/components/Admin/Admin.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-
+import { Link, Outlet, Navigate } from 'react-router-dom';
 const Admin = () => {
+  const user = {role: "user"};
+
+  if(user.role !== "admin"){
+    return <Navigate to='/' />;
+  }
+
   return (
        <div>
         
        <h2>Admin Panel</h2> 
-        
+         <ul>
+          <li>
+            <Link to="/admin/sales">Sales</Link>
+          </li>
+           <li>
+            <Link to="/admin/sellers">Sellers</Link>
+          </li>
+         </ul>
+         <Outlet />
         </div>
   )
 }
--- route_templates/src/components/Articles/Articles.jsx
+++ route_templates/src/components/Articles/Articles.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-
+import { useSearchParams } from 'react-router-dom';
 
 const Articles = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy");
+  const category = searchParams.get("category");
+
+  const handleSortBy = () =>{
+      setSearchParams({
+        sortBy: "views",
+        category: "category"
+      });
+  }
   return (
        <div>
         
        <h2>Articles
-        <p>SortBy: Category:</p>
+           <p>
+          SortBy: {sortBy} Category: {category}
+          </p>
         </h2> 
-        
+        <button onClick={handleSortBy}> Sort by views</button>
         </div>
   )
 }
