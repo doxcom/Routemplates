63d82b0ec904300f21a2001b2147e5d9652ca1f0
doxcom <aldoperez1319@gmail.com>
2026-07-16 17:51:44 -0600

    adding json call fetch

--- route_templates/src/components/Admin/Sellers.jsx
+++ route_templates/src/components/Admin/Sellers.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const Sellers = () => {
+  const [name, setName] = useState("");
+ /* useEffect(() =>{
+    // console.log("component mount!");
+    const notification = 5;
+    document.title = `Name: ${name}`; //to put title on browser tab, also we can use restore title mode to clean up tab title after jumping in another section
+  }, [name]);
+*/
+
+ //section for API purposes:
+
+    useEffect(() =>{
+       axios.get("https://jsonplaceholder.typicode.com/users").then(res =>
+        console.log(res))
+    }, [])
+
   return (
-     
-        
-       <h2>Admin Sellers Page</h2> 
-        
-       
-  )
+    <>
+       <h3>Admin Sellers Page</h3> 
+       <input type='text' onChange={(e) => setName(e.target.value)} />
+    </>
+     )
 }
 
 export default Sellers;
