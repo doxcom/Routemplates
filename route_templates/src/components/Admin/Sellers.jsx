2e939082e07d5714cee133e8c56fc32bfd82d387
doxcom <aldoperez1319@gmail.com>
2026-07-17 14:40:25 -0600

    adding async wait and loading prompt

--- route_templates/src/components/Admin/Sellers.jsx
+++ route_templates/src/components/Admin/Sellers.jsx
@@ -1,8 +1,12 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
+import Loader from '../Common/Loader';
 
 const Sellers = () => {
   const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState("")
+  const [sellers, setSellers] = useState([]);
  /* useEffect(() =>{
     // console.log("component mount!");
     const notification = 5;
@@ -11,17 +15,48 @@ const Sellers = () => {
 */
 
  //section for API purposes:
+    useEffect(() => {
+       fetchSellers();
+      /*
+                         setIsLoading(true)
+                         axios.get("https://jsonplaceholder.typicode.com/users")
+                              .then((res) => {
+                                                setSellers(res.data)
+                                                setIsLoading(false)
+                                              }
+                                    ).catch(err => 
+                                      {
+                                       setIsLoading(false);
+                                       setErrors(err.message);
+                                       }); 
+                    */
+                      },[]);
 
-    useEffect(() =>{
-       axios.get("https://jsonplaceholder.typicode.com/users").then(res =>
-        console.log(res))
-    }, [])
+  const fetchSellers = async () =>{
+      setIsLoading(true)
+      try {
+             const res = await axios.get("https://jsonplaceholder.typicode.com/userss");
+             setSellers(res.data);
+             setIsLoading(false);
+          } catch (err) {
+            setIsLoading(false);
+            setErrors(err.message);
+          }
+      
+  }
+
+  // if(isLoading) return <h3>Loading...</h3>;                    
 
   return (
     <>
        <h3>Admin Sellers Page</h3> 
        <input type='text' onChange={(e) => setName(e.target.value)} />
-    </>
+       {isLoading && <Loader/>}
+       {errors && <em>{errors}</em>}
+       {sellers.map((seller) => (
+          <p key={seller.id}>{seller.name}</p>
+          ))}
+       </>
      )
 }
 
